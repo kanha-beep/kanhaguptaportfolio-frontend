9eb223a4300baa205a15aef4f3ea547c6d1d52e2
kanha-beep <kanhashree2223@gmail.com>
2025-11-14 13:05:53 +0530

    url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-const API_URL = process.env.API_URL
+const API_URL = process.env.API_URL || "http://localhost:3000/api"
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: API_URL,
   withCredentials: true,
 });
 
